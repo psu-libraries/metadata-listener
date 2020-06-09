d6e39b0a1222fa8fe9e771c573ac33984125d8c0
Dann Bohn <djb44@psu.edu>
2020-06-09 15:00:53 -0400

    remove cruft

---
+++ lib/clam.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rufus-scheduler'
+require 'clamby'
+
+scheduler = Rufus::Scheduler.new
+
+scheduler.every '2h' do
+  Clamby.update
+end
+
+class ClamUtils
+  def scan(filename)
+    Clamby.configure(daemonize: false)
+    virus_found = Clamby.virus?(filename)
+    return true if virus_found
+
+    false
+  end
+end
