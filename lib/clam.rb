c36c3d42372b748130ab276e339ee523492d2426
Dann Bohn <djb44@psu.edu>
2020-06-09 14:50:25 -0400

    remove logging for now; this will get refactored anyway

--- lib/clam.rb
+++ lib/clam.rb
@@ -5,10 +5,8 @@ require 'clamby'
 require_relative '../config/log'
 
 scheduler = Rufus::Scheduler.new
-logger = Logger.new(STDOUT)
 
 scheduler.every '2h' do
-  logger.info('Updating ClamAV Database')
   Clamby.update
 end
 
