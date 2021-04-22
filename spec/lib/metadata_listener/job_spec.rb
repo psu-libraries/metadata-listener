9c1f1807d128d0db542fa0a8d7173af551380a95
Adam Wead <awead@users.noreply.github.com>
2021-04-22 08:41:36 -0400

    Metadata reporting services
    Creates a second service, similar to VirusReportingServices, that calls
    Fits to extract metadata from the file and send the results to the
    Scholarsphere API.
    
    Refactors MetadataListener::Job to allow specifying which reports to run
    and submit to Scholarsphere.

--- spec/lib/metadata_listener/job_spec.rb
+++ spec/lib/metadata_listener/job_spec.rb
@@ -1,15 +1,32 @@
 # frozen_string_literal: true
 
 RSpec.describe MetadataListener::Job do
+  let(:path) { upload_file(file: fixture_path.join('1.pdf')) }
+
+  before do
+    allow(MetadataListener::VirusReportingService).to receive(:call)
+    allow(MetadataListener::MetadataReportingService).to receive(:call)
+  end
+
   it { is_expected.to be_an(ActiveJob::Base) }
 
-  context 'when everything goes according to plan' do
-    let(:path) { upload_file(file: fixture_path.join('1.pdf')) }
-
-    it 'downloads the file and call the VirusReportingService' do
-      allow(MetadataListener::VirusReportingService).to receive(:call)
+  context 'without requesting any specific services' do
+    it 'calls the VirusReportingService by default' do
       described_class.perform_now(path: path)
+      expect(MetadataListener::MetadataReportingService).not_to have_received(:call)
       expect(MetadataListener::VirusReportingService).to have_received(:call).with(
+        path: a_kind_of(String),
+        endpoint: nil,
+        api_token: nil
+      )
+    end
+  end
+
+  context 'when requesting a metadata report' do
+    it 'calls the Metadata report' do
+      described_class.perform_now(path: path, services: [:metadata])
+      expect(MetadataListener::VirusReportingService).not_to have_received(:call)
+      expect(MetadataListener::MetadataReportingService).to have_received(:call).with(
         path: a_kind_of(String),
         endpoint: nil,
         api_token: nil
