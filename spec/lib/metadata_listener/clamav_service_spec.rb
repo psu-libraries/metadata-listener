950446245fa68816f8988504eb24404acce6c29a
Adam Wead <awead@users.noreply.github.com>
2021-04-30 14:41:38 -0400

    Call Clamby::Command directly
    Because the Clamby gem is supporting legacy behavior, if clamscan
    encounters an unknown error, Clamby will indicate that the file has a
    virus, when in fact, it may not. This is a false-positive case, and the
    legacy behavior takes a slightly paranoid approach. While this is
    understandable, it creates problems on our end, and we'd rather know
    that file caused an error when scanned and not that it "has a virus."
    
    To get around this, we're calling the same methods that the gem does and
    handling the errors in our own way. An exit code of 0 means "no virus",
    an exit code of 1 means "definitely virus", and anything else raises an
    error and we figure it out later, re-try, etc.

--- spec/lib/metadata_listener/clamav_service_spec.rb
+++ spec/lib/metadata_listener/clamav_service_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe MetadataListener::ClamavService do
-  subject { described_class.call(path) }
+  subject(:service) { described_class.call(path) }
 
   context 'when a virus is not present' do
     let(:path) { fixture_path.join('1.pdf').to_s }
@@ -19,5 +19,19 @@ RSpec.describe MetadataListener::ClamavService do
     let(:path) { 'nothere' }
 
     it { is_expected.to be(false) }
+  end
+
+  context 'when clamscan returns a different exit code' do
+    let(:path) { fixture_path.join('1.pdf').to_s }
+    let(:mock_command) { instance_spy('Clamby::Command') }
+
+    before do
+      allow(Clamby::Command).to receive(:new).and_return(mock_command)
+      allow(mock_command).to receive(:run) { system('exit 2', out: File::NULL) }
+    end
+
+    it 'raises an error' do
+      expect { service }.to raise_error(Clamby::ClamscanClientError)
+    end
   end
 end
