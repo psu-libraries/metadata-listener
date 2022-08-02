b0a50c0f2f966137a1b4e4e18cf3d63a71b1360f
Dann <whereismyjetpack@users.noreply.github.com>
2022-08-02 09:59:01 -0400

    Update fits / tika (#75)
    * updates fits
    
    * updates tika

--- spec/lib/metadata_listener/service/tika_spec.rb
+++ spec/lib/metadata_listener/service/tika_spec.rb
@@ -7,21 +7,21 @@ RSpec.describe MetadataListener::Service::Tika do
     let(:path) { fixture_path.join('1.pdf').to_s }
 
     its(:text) { is_expected.to include('pdftestarticle') }
-    its(:metadata) { is_expected.to include('Author' => 'Florian Grandel') }
+    its(:metadata) { is_expected.to include('dc:creator' => 'Florian Grandel') }
   end
 
   context 'with a Word document' do
     let(:path) { fixture_path.join('1.docx').to_s }
 
     its(:text) { is_expected.to include('chambray') }
-    its(:metadata) { is_expected.to include('Application-Name' => 'Microsoft Office Word') }
+    its(:metadata) { is_expected.to include('extended-properties:Application' => 'Microsoft Office Word') }
   end
 
   context 'with a PowerPoint document' do
     let(:path) { fixture_path.join('1.pptx').to_s }
 
     its(:text) { is_expected.to include('chambray') }
-    its(:metadata) { is_expected.to include('Application-Name' => 'Microsoft Macintosh PowerPoint') }
+    its(:metadata) { is_expected.to include('extended-properties:Application' => 'Microsoft Macintosh PowerPoint') }
   end
 
   context 'with a non-text document' do
